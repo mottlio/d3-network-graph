f15fa4d9328a9f892f32d54fb2e2ee28db2f3aed
Michal Mottl <michal.mottl@coleurope.eu>
2018-05-21 13:13:54 +0700

    prepare csv for use with d3 using formatter, make array of links

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Using a D3 force directed graph to visualize relationships between members of the US Senate
+
+1st step -> prepare the CSV data for use with D3 by using a FORMATTER
+
+2nd step -> using a callback (3rd argument to d3.csv) which accepts data from the formatter ("nodes") and first creates links between different members of the senate based on which committees they are in (with source and target value based on the members' names)
+
+There are 1021 links!!!
+
--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 d3.csv('./senate_committee_data.csv', function(d, i, headers) {
     var committees = headers.slice(2).filter(h => d[h] === '1');
     return {
-      name: d.name,
-      party: d.party,
-      committees: committees
+    name: d.name,
+    party: d.party,
+    committees: committees
     }
-  }, function(error, nodes) {
+}, function(error, nodes) {
     if (error) throw error;
-  
-  });
+
+console.log(makeLinks(nodes));
+
+function makeLinks(nodes){
+    var links = [];
+    for (var i = 0; i < nodes.length; i++) {
+        for (var j = i + 1; j < nodes.length; j++) {
+            var s1 = nodes[i];
+            var s2 = nodes[j];
+            for(var k = 0; k < s1.committees.length; k++){
+                var committee = s1.committees[k];
+                if(s2.committees.includes(committee)){
+                    links.push({
+                        source: s1.name,
+                        target: s2.name
+                    });
+                    break;
+                }
+            }
+        }
+    }
+    return links;
+}
+});
